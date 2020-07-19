a3c97cbe97b7ada70e5d983bad9645da01da8bc5
taher latreche <taherhehe@gmail.com>
2020-07-19 20:53:42 +0200

    security and sanetization

--- app.js
+++ app.js
@@ -5,25 +5,53 @@ const Users = require("./routes/users");
 const Signup = require("./routes/signup");
 const cors = require("cors");
 const Profile = require("./routes/profile");
-const app = express();
+const rateLimit = require("express-rate-limit");
 const morgan = require("morgan");
 const errHandler = require("./controller/errConrtoller");
-if (process.env.NODE_ENV === "dev") app.use(morgan("dev"));
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const hpp = require("hpp");
 
+const app = express();
+// security headers against noSQL injection
+app.use(helmet());
+//data sanitazation
+app.use(mongoSanitize());
+//data sanitazation against xss
+app.use(xss());
+if (process.env.NODE_ENV === "dev") app.use(morgan("dev"));
+// limit request for the api
+const limiter = rateLimit({
+  max: 500,
+  windowMs: 60 * 60 * 1000,
+  message: "too many request from this ip , plz try again later",
+});
+app.use(limiter);
+// body parser
 app.use(
   bodyParser.urlencoded({
     extended: false,
   })
 );
+// dublicate params
+// app.use(
+//   hpp({
+//     whitelist: ["duration"],
+//   })
+// );
+app.use(hpp());
+
 app.use(bodyParser.json());
 
 app.use(cors());
+// static serving
 app.use("/images", express.static("profile-imgss"));
 //my routes
 app.use("/users", Users);
 app.use("/profile", Profile);
 app.use("/signup", Signup);
-
+// not found
 app.all("*", (req, res, next) => {
   next(new appError(`cant find ${req.originalUrl} on this server`, 404));
 });
--- controller/authController.js
+++ controller/authController.js
@@ -15,6 +15,15 @@ const signToken = id => {
 
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
+  const cookieOptions = {
+    expires: new Date(Date.now + process.env.JWT_COOKIE_IN * 3600),
+
+    httpOnly: true,
+  };
+
+  if (process.env.NODE_ENV === "prod") cookieOptions.secure = true;
+
+  res.cookie("jwt", token, cookieOptions);
 
   res.status(statusCode).json({
     status: "success",
